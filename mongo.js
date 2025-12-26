da60bbd602d73375bcc31c92f5b5bcee7a7c0446
z0048hzr <vitaly.zayko@siemens-energy.com>
2025-12-26 23:22:19 +0100

    Completing 3.12, adding MongoDB entires via command line

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3 || process.argv.length > 5) {
+  console.log('give DB password, name and phone number as arguments. But not more!')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url = `mongodb+srv://vitalyzayko:${password}@learningfullstack.jrspuux.mongodb.net/phonebookDB?appName=LearningFullstack`
+
+// mongodb+srv://vitalyzayko:<db_password>@learningfullstack.jrspuux.mongodb.net/?appName=LearningFullstack
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: newName,
+  number: newNumber,
+})
+
+/* Person.find({}).then(result => {
+  result.forEach(person => {
+    console.log(person)
+  })
+  mongoose.connection.close()
+}) */
+
+person.save().then(result => {
+  //console.log("result:", result)
+  console.log("added", result.name, "number", result.number, "to phonebook")
+  mongoose.connection.close()
+})
